4726928b46caf2304a9b8f7ff7e77d69aaefb878
alex beer <alexander.b.beer@gmail.com>
2014-07-23 14:23:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Everoam2::Application.config.secret_key_base = '5d11aee50922f40aa371ff209aa0ad3da7e1c61adac5cc853ebbb77de0719628ad55f88d32405ae46d2a87f44134200c0860ffec0c32557f77c641bb7896e1ad'
