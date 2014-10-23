1961ae5e25d33b935ddb88331b134f723e01839d
alex beer <alexander.b.beer@gmail.com>
2014-10-23 13:25:09 -0400

    validates title

--- app/models/roam.rb
+++ app/models/roam.rb
@@ -8,31 +8,12 @@ class Roam < ActiveRecord::Base
   	validates :user_id, presence: true
 
 	has_attached_file :image, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
-		validates :images, presence: true
-
-	has_attached_file :imagetwo, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
 
 
-	has_attached_file :imagethree, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
+	validates :title, presence: true
 
 
-	has_attached_file :imagefour, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
 
 
-	has_attached_file :imagefive, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
 
-
-	has_attached_file :imagesix, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
-
-
-	has_attached_file :imageseven, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
-
-
-	has_attached_file :imageeight, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
-
-
-	has_attached_file :imagenine, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
-
-
-	has_attached_file :imageten, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
 end
