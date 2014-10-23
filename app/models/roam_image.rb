ce65a88ea75f4fbf5bbb3b263964ac388bd05d12
alex beer <alexander.b.beer@gmail.com>
2014-10-23 13:22:29 -0400

    took away validations

--- app/models/roam_image.rb
+++ app/models/roam_image.rb
@@ -1,6 +1,4 @@
 class RoamImage < ActiveRecord::Base
   belongs_to :roam
-  validates :image_large_url, presence: true
-  validates :image_medium_url, presence: true
-  validates :image_thumb_url, presence: true
+  
 end
